86bf58344d2172c5aa19878711dc89409198e18d
greg_huang <greg_huang@trend.com.tw>
2017-01-13 18:53:58 +0800

    Initialize IntergrationSpec

--- core/src/test/scala/com/trend/spn/IntegrationSpec.scala
+++ core/src/test/scala/com/trend/spn/IntegrationSpec.scala
@@ -1,23 +1,21 @@
 package com.trend.spn
 
-import akka.stream.ActorMaterializer
-import akka.stream.testkit.StreamSpec
-import com.typesafe.config.ConfigFactory
-import net.manub.embeddedkafka.EmbeddedKafkaConfig
-import org.scalatest.BeforeAndAfterEach
+import com.typesafe.config.{Config, ConfigFactory}
 
 /**
  * Created by GregHuang on 1/13/17.
  */
-class IntegrationSpec extends StreamSpec with BeforeAndAfterEach {
-    val props =
+class IntegrationSpec(props: Config) extends FileSourceKafkaSpec(props) {
+    def this() = this(
         ConfigFactory.parseString( """
-        akka.kafka.consumer.kafka-clients.bootstrap.servers = "localhost:9092"
-        akka.kafka.consumer.topic = test01""")
-        .withFallback(ConfigFactory.load())
+          akka.kafka.consumer{
+            kafka-clients.bootstrap.servers = "localhost:9092"
+            topic = test01
+            sourcePath = /ad_proc_100_test.txt
+          }
+                                   """)
+          .withFallback(ConfigFactory.load())
+    )
 
-    implicit val config = props.getConfig("akka.kafka.consumer")
-    implicit val mat = ActorMaterializer()(system)
-    implicit val ec = system.dispatcher
-    implicit val embeddedKafkaConfig = EmbeddedKafkaConfig(9092, 2181)
+
 }
